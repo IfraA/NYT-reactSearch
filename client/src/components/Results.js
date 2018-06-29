481aa64106101092683b4458b38646aac4df668f
IfraA <ifraa.aijaz@gmail.com>
2018-06-29 14:55:01 -0700

    files

---
+++ client/src/components/Results.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+//renders date into readable format for users
+const dateRender = function (date) {
+    let newDate = date.slice(0, 10);
+    return newDate;
+};
+
+const Results = props => (
+    <div className="container">
+        <li className="list-group-item">
+            <h4>
+                <span>
+                    <em>{props.title}</em>
+                </span>
+                <span className="float-right">
+                    <a href={props.url} target="_blank">
+                        <button className="btn btn-primary ">
+                            <i className="fas fa-external-link-alt" />
+                        </button>
+                    </a>
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => props.handleSaveButton(props._id)}
+                    >
+                        <i className="fas fa-download" />
+                    </button>
+                </span>
+            </h4>
+            <p>Published: {dateRender(props.date)}</p>
+        </li>
+    </div>
+);
+
+export default Results;
---
+++ client/src/components/Saved.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+//renders date into readable format for users
+const dateRender = function (date) {
+    let newDate = date.slice(0, 10);
+    return newDate;
+};
+
+const Saved = props => (
+    <div className="container">
+        <li className="list-group-item">
+            <h4>
+                <span>
+                    <em>{props.title}</em>
+                </span>
+                <span className="float-right">
+                    <button className="btn btn-warning ">
+                        <a href={props.url} target="_blank">
+                            <i className="fas fa-external-link-alt" />
+                        </a>
+                    </button>
+
+                    <button
+                        className="btn btn-warning"
+                        onClick={() => props.handleDeleteButton(props._id)}
+                    >
+                        <i className="fas fa-trash-alt" />
+                    </button>
+                </span>
+            </h4>
+            <p>Published: {dateRender(props.date)}</p>
+        </li>
+    </div>
+);
+
+export default Saved;
